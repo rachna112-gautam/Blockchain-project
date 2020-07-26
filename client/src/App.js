dee2d2c537ca0dc9e74024b6fcb5b7323877499a
rachna112-gautam <rachna01121999@gmail.com>
2020-07-26 15:36:09 +0530

    bootstrap added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import Mycontract from 'contracts/MyContract.json';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+       <h1 className="text-center">Welcome</h1>
+       <button className="btn btn-outline-success">Click here</button>
     </div>
   );
 }
