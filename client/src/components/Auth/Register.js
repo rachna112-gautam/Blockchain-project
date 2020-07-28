9c543ffb10370b871e2ccfd6b52bdff951b490ca
rachna112-gautam <rachna01121999@gmail.com>
2020-07-28 10:42:59 +0530

    Registration setup

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
-    return (
-        <div class="card text-center">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-  <div class="card-footer text-muted">
-    2 days ago
-  </div>
-</div>
-    )
-}
+  return (
+    <div class="card text-center card-dark bg-dark" >
+      <div class="card-header "></div>
+      <div class="card-body">
+        <h5 class="card-title">Welcome to blockchain.com</h5>
+        <p class="card-text">
+        Check the ID of your invitee before you proceeding further...
+        </p>
+        <input class="form-control ml-10 mr-10" type="email" placeholder="Enter Your Mail"></input>
+        <a href="#" class="btn btn-primary mt-4">
+         OK
+        </a>
+      </div>
+      <div class="card-footer text-muted">You are using the Referral ID : <input placeholder="Referral ID" required type="number" value="" min="1" max="100"/></div>
+    </div>
+  );
+};
 
 export default Register;
--- client/src/components/Home/Welcome.js
+++ client/src/components/Home/Welcome.js
@@ -1,17 +1,15 @@
 import  React, { Component} from 'react';
 import '../../App.css';
-import { Redirect } from 'react-router-dom';
+
 
 
 const Welcome = () => {
-    function  onClickHandle() {
-        return <Redirect to='/register'></Redirect>
-    }
+
     return (
         <div className="text">
           <h1>WELCOME TO DAPP</h1>
           <p>Total Users in the system </p>
-          <button className="btn" onClick={onClickHandle}>Register</button>
+          <a className="reg-btn" href="register"></a>
           </div>
     )
 }
