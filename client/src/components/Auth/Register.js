89ece067d8a74e6c67aa57af9047d90eda9558e1
rachna112-gautam <rachna01121999@gmail.com>
2020-07-28 10:54:02 +0530

    ..

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,16 +1,16 @@
 import React from "react";
-
+import './../../App.css';
 const Register = () => {
   return (
     <div class="card text-center card-dark bg-dark" >
-      <div class="card-header "></div>
-      <div class="card-body">
+      <div class="card-header text-center"></div>
+      <div class="card-body text-center col-sm-6">
         <h5 class="card-title">Welcome to blockchain.com</h5>
         <p class="card-text">
         Check the ID of your invitee before you proceeding further...
         </p>
         <input class="form-control ml-10 mr-10" type="email" placeholder="Enter Your Mail"></input>
-        <a href="#" class="btn btn-primary mt-4">
+        <a href="#" class="btn btn-secondary mt-4">
          OK
         </a>
       </div>
