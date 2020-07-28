05037a3271bbbdba0cc6e6327fef1b9f6bd05c2a
rachna112-gautam <rachna01121999@gmail.com>
2020-07-28 12:28:17 +0530

    .

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -7,14 +7,14 @@ const Register = () => {
       <div class="card-body text-center col-sm-6">
         <h5 class="card-title">Welcome to blockchain.com</h5>
         <p class="card-text">
-        Check the ID of your invitee before you proceeding further...
+        Check the ID of your invitee before proceeding further...
         </p>
         <input class="form-control ml-10 mr-10" type="email" placeholder="Enter Your Mail"></input>
         <a href="#" class="btn btn-secondary mt-4">
          OK
         </a>
       </div>
-      <div class="card-footer text-muted">You are using the Referral ID : <input placeholder="Referral ID" required type="number" value="" min="1" max="100"/></div>
+      <div class="card-footer text-muted">You are using the Referral ID : <input placeholder="Referral ID" required type="number"  min="1" max="100"/></div>
     </div>
   );
 };
